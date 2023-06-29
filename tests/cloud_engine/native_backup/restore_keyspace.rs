11ebcd05549ec56846594f8f2a0f3d5bcb407c37
Ping Yu <yuping@pingcap.com>
2023-06-29 19:42:09 +0800

    native_br,tests: Improve integration tests for Native BR. (#884)
    Signed-off-by: Ping Yu <yuping@pingcap.com>

--- tests/cloud_engine/native_backup/restore_keyspace.rs
+++ tests/cloud_engine/native_backup/restore_keyspace.rs
@@ -1,6 +1,12 @@
 // Copyright 2023 TiKV Project Authors. Licensed under Apache-2.0.
 
-use std::{sync::Arc, time::Duration};
+use std::{
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+    time::Duration,
+};
 
 use kvengine::dfs::{DFSConfig, S3Fs};
 use kvproto::metapb;
@@ -28,22 +34,31 @@ const BASIC_DATA_COUNT: usize = 10;
 const RANDOM_VALUE_LEN: usize = 128;
 const NODES_COUNT: usize = 4;
 const KEYSPACE_COUNT: usize = 3;
-const DEFAULT_LOOP_COUNT: usize = 3;
+const DEFAULT_LOOP_COUNT: usize = 5;
+
+// The numbers have no special meaning, just to make them different.
+// Require to be larger than 136, see `i_to_val`.
+const BASIC_DATA_LEN: usize = 140;
+const IMPORT_DATA_LEN: usize = 142;
+const PRE_BACKUP_DATA_LEN: usize = 144;
+const POST_BACKUP_DATA_LEN: usize = 146;
+const PRE_PITR_DATA_LEN: usize = 148;
+const FINAL_DATA_LEN: usize = 150;
 
 #[test]
 fn test_restore_keyspace() {
     test_util::init_log_for_test();
-    // wal sync dir does not matter with pitr, no need to do parameter matrix
-    test_restore_keyspace_opt(false, false); // TODO: remove when wal sync dir is enabled by default.
-    test_restore_keyspace_opt(true, true);
-}
-
-fn test_restore_keyspace_opt(enable_wal_sync_dir: bool, pitr: bool) {
     let loop_count = std::env::var("LOOP")
         .unwrap_or_default()
         .parse::<usize>()
         .unwrap_or(DEFAULT_LOOP_COUNT);
 
+    test_restore_keyspace_opt(loop_count, true);
+    // Regression test for `inner_key_off` disabled.
+    test_restore_keyspace_opt(DEFAULT_LOOP_COUNT, false);
+}
+
+fn test_restore_keyspace_opt(loop_count: usize, enable_inner_key_off: bool) {
     let cases = vec![
         // keyspace_id, data_count, shuffle_regions, has_learner, loop_count
         (1, 1, None, false, 1),
@@ -57,18 +72,17 @@ fn test_restore_keyspace_opt(enable_wal_sync_dir: bool, pitr: bool) {
         .prefix("test_restore_keyspace_")
         .tempdir()
         .unwrap();
-    let base_dir_str = base_dir.path().to_str().unwrap();
 
     let oss_dir = base_dir.path().join("oss");
     let mut oss = ObjectStorageService::new(oss_dir);
     oss.start_server();
 
     let dfs_config = DFSConfig {
-        prefix: "test_restore_keyspace_".to_string(),
+        prefix: "pfx".to_string(),
         s3_endpoint: format!("http://127.0.0.1:{}", oss.port()),
         s3_key_id: "admin".to_string(),
         s3_secret_key: "admin".to_string(),
-        s3_bucket: "test_restore_keyspace_".to_string(),
+        s3_bucket: "bkt".to_string(),
         s3_region: "local".to_string(),
         zstd_compression_level: "3".to_string(),
         ..Default::default()
@@ -76,7 +90,7 @@ fn test_restore_keyspace_opt(enable_wal_sync_dir: bool, pitr: bool) {
 
     let mut cluster = ServerCluster::new(
         alloc_node_id_vec(NODES_COUNT),
-        |node_id: u16, conf: &mut TikvConfig| {
+        |_, conf: &mut TikvConfig| {
             conf.dfs = dfs_config.clone();
             // Set small mem-table size to make data reach L1 and generate over bound
             // shards.
@@ -84,13 +98,7 @@ fn test_restore_keyspace_opt(enable_wal_sync_dir: bool, pitr: bool) {
             conf.coprocessor.region_split_size = ReadableSize::kb(128); // kv_opts.base_size = 8kb
             conf.coprocessor.region_bucket_size = ReadableSize::kb(64);
             conf.rfengine.target_file_size = ReadableSize::mb(1);
-            conf.rfengine.wal_sync_dir = enable_wal_sync_dir
-                .then(|| {
-                    let dir = format!("{}/wal_sync/{}", base_dir_str, node_id);
-                    step!("enable wal sync dir: {}", dir);
-                    dir
-                })
-                .unwrap_or_default();
+            conf.enable_inner_key_offset = enable_inner_key_off;
         },
     );
     cluster.wait_region_replicated(&[], 3);
@@ -108,7 +116,7 @@ fn test_restore_keyspace_opt(enable_wal_sync_dir: bool, pitr: bool) {
         client.put_kv(
             0..BASIC_DATA_COUNT,
             gen_keyspace_key(keyspace_id as u32),
-            i_to_val_138,
+            i_to_val(BASIC_DATA_LEN),
         );
     }
 
@@ -143,7 +151,6 @@ fn test_restore_keyspace_opt(enable_wal_sync_dir: bool, pitr: bool) {
                 shuffle_regions,
                 has_learner,
                 &runtime,
-                pitr,
             );
         }
     }
@@ -162,9 +169,9 @@ fn test_restore_keyspace_impl(
     shuffle_regions: Option<usize>,
     has_learner: bool,
     runtime: &Runtime,
-    pitr: bool,
 ) {
     step!("case: {case_name}");
+    let mut rng = rand::thread_rng();
     let mut client = cluster.new_client();
     let i_to_key = gen_keyspace_key(keyspace_id);
     let (keyspace_start, keyspace_end) = (
@@ -172,51 +179,71 @@ fn test_restore_keyspace_impl(
         get_keyspace_prefix(keyspace_id + 1),
     );
 
-    // Import data.
-    client.put_kv(0..data_count, &i_to_key, i_to_val_140);
-    let origin_ref_store = client.dump_ref_store();
-
-    // Execute backup.
-    let backup_name = format!("restore_keyspace_test_{}", rand::thread_rng().gen::<u64>());
     let backup_config = backup::BackupConfig {
         dfs: dfs_config.clone(),
         skip_keyspace_meta: true,
         ..Default::default()
     };
-    let truncate_ts = if pitr {
+    let s3fs = Arc::new(S3Fs::new(
+        dfs_config.prefix.clone(),
+        dfs_config.s3_endpoint.clone(),
+        dfs_config.s3_key_id.clone(),
+        dfs_config.s3_secret_key.clone(),
+        dfs_config.s3_region.clone(),
+        dfs_config.s3_bucket.clone(),
+    ));
+    let reporter = Arc::new(DummyStepReporter::default());
+
+    // Import data.
+    client.put_kv(0..data_count, &i_to_key, i_to_val(IMPORT_DATA_LEN));
+    let origin_ref_store = client.dump_ref_store();
+
+    // Prepare for PiTR.
+    let truncate_ts = if rng.gen_ratio(1, 3) {
         let ts = client.get_ts();
-        client.put_kv(0..data_count, &i_to_key, i_to_val_142);
+        client.put_kv(0..data_count, &i_to_key, i_to_val(PRE_PITR_DATA_LEN));
         step!("another writes for pitr done");
         client.verify_data_with_ref_store();
         Some(ts.into_inner())
     } else {
         None
     };
-    let backup_ts = client.get_ts().into_inner();
-    // Put more data before backup to verify truncate ts take affect.
-    client.put_kv(0..data_count, &i_to_key, i_to_val_142);
-    step!("another writes done");
-    client.verify_data_with_ref_store();
-    assert!(
-        client
-            .verify_data_with_given_ref_store(&origin_ref_store, None, &RequestOptions::default())
-            .is_err(),
-        "case: {}",
-        case_name,
-    );
-    step!("verify before backup ok");
-    let (_, backup_meta) = backup::backup_cluster_with_ts(
-        backup_config,
-        false,
-        backup_name.clone(),
-        cluster.get_pd_client().as_ref(),
-        backup_ts,
-        None,
-    )
-    .expect("backup::backup_cluster");
-    info!("backup_cluster result: {:?}", backup_meta);
-    client.verify_data_with_ref_store();
-    step!("backup done");
+
+    // Perform snapshot backup.
+    let snapshot_backup_name = generate_backup_name();
+    let backup_meta = {
+        let backup_ts = client.get_ts().into_inner();
+        // Put more data before backup to verify truncate ts take affect.
+        client.put_kv(0..data_count, &i_to_key, i_to_val(PRE_BACKUP_DATA_LEN));
+        step!("another writes done");
+        client.verify_data_with_ref_store();
+        assert!(
+            client
+                .verify_data_with_given_ref_store(
+                    &origin_ref_store,
+                    None,
+                    &RequestOptions::default()
+                )
+                .is_err(),
+            "case: {}",
+            case_name,
+        );
+        step!("verify before backup ok");
+        let (_, backup_meta) = backup::backup_cluster_with_ts(
+            backup_config.clone(),
+            false,
+            snapshot_backup_name.clone(),
+            cluster.get_pd_client().as_ref(),
+            backup_ts,
+            None,
+        )
+        .expect("backup::backup_cluster");
+        info!("backup_cluster result: {:?}", backup_meta);
+        client.verify_data_with_ref_store();
+        step!("backup done");
+
+        backup_meta
+    };
 
     // Shuffle regions.
     if let Some(shuffle_regions) = shuffle_regions {
@@ -224,7 +251,7 @@ fn test_restore_keyspace_impl(
         let region_count = client.pd_client.get_regions_number();
         let mut i = 0;
         while i < shuffle_regions {
-            let split_key = rand::thread_rng().gen::<usize>() % data_count;
+            let split_key = rng.gen::<usize>() % data_count;
             if let Err(e) = client.try_split(&i_to_key(split_key)) {
                 warn!("try split error: {:?}", e);
                 continue;
@@ -236,7 +263,7 @@ fn test_restore_keyspace_impl(
         let split_region_count = client.pd_client.get_regions_number();
 
         for _ in 0..shuffle_regions {
-            let source_key = rand::thread_rng().gen::<usize>() % data_count;
+            let source_key = rng.gen::<usize>() % data_count;
             if let Err(e) = client.try_merge_adjacent_region(
                 &i_to_key(source_key),
                 Some(&keyspace_prefix),
@@ -256,7 +283,7 @@ fn test_restore_keyspace_impl(
     }
 
     // Put another data.
-    client.put_kv(0..data_count, &i_to_key, i_to_val_142);
+    client.put_kv(0..data_count, &i_to_key, i_to_val(POST_BACKUP_DATA_LEN));
     step!("another writes done");
     client.verify_data_with_ref_store();
     assert!(
@@ -268,64 +295,103 @@ fn test_restore_keyspace_impl(
     );
     step!("verify ok");
 
+    // Test for "PiTR on restored data". For the following scenario:
+    // T0: put data0.
+    // T1: perform backup.
+    // T2: put data1.
+    // T3: restore to backup. Now data0 should be in store.
+    // T4: PiTR to T2. Now data1 should be in store.
+    let truncate_ts_pitr = client.get_ts().into_inner();
+    let ref_store_pitr = client.dump_ref_store();
+
+    // Instant backup before restore
+    let instant_backup_name = {
+        let backup_ts = client.get_ts().into_inner();
+        let instant_backup_name = generate_backup_name();
+        let (_, backup_meta) = backup::backup_cluster_with_ts(
+            backup_config,
+            true,
+            instant_backup_name.clone(),
+            cluster.get_pd_client().as_ref(),
+            backup_ts,
+            Some(backup_meta),
+        )
+        .expect("backup::backup_cluster");
+        info!("backup_cluster result (instant): {:?}", backup_meta);
+        step!("backup for pitr done");
+        instant_backup_name
+    };
+
     // Restore keyspace.
-    let dfs_config = dfs_config.clone();
-    let s3fs = Arc::new(S3Fs::new(
-        dfs_config.prefix,
-        dfs_config.s3_endpoint,
-        dfs_config.s3_key_id,
-        dfs_config.s3_secret_key,
-        dfs_config.s3_region,
-        dfs_config.s3_bucket,
-    ));
-    let reporter = Arc::new(DummyStepReporter::default());
     restore_keyspace::restore_keyspace(
         keyspace_id,
         keyspace_id,
-        &backup_name,
+        &snapshot_backup_name,
         None,
-        s3fs,
+        s3fs.clone(),
         cluster.get_pd_client(),
         runtime,
         truncate_ts,
-        reporter,
+        reporter.clone(),
     )
     .unwrap();
     step!("restore done");
 
     // Verify restored data.
-    // Retry as shard restore is asynchronously applied.
-    let mut verify_options: Vec<(Option<(&[u8], &[u8])>, RequestOptions)> =
-        vec![(None, RequestOptions::default())];
-    if has_learner {
-        verify_options.push((
-            Some((&keyspace_start, &keyspace_end)),
-            RequestOptions {
-                peer_role: RequestPeerRole::Learner,
-            },
-        ));
-    }
-    let mut verify_res = Err("unknown".into());
-    for (range, opt) in verify_options {
-        let ok = try_wait(
-            || {
-                verify_res =
-                    client.verify_data_with_given_ref_store(&origin_ref_store, range, &opt);
-                verify_res.is_ok()
-            },
-            10,
-        );
-        if !ok {
-            client
-                .verify_data_with_given_ref_store(&origin_ref_store, range, &opt)
-                .unwrap();
+    {
+        let mut verify_options: Vec<(Option<(&[u8], &[u8])>, RequestOptions)> =
+            vec![(None, RequestOptions::default())];
+        if has_learner {
+            verify_options.push((
+                Some((&keyspace_start, &keyspace_end)),
+                RequestOptions {
+                    peer_role: RequestPeerRole::Learner,
+                },
+            ));
         }
+        let mut verify_res = vec![];
+        for (range, opt) in verify_options {
+            verify_res.push(
+                client
+                    .verify_data_with_given_ref_store(&origin_ref_store, range, &opt)
+                    .unwrap(),
+            );
+        }
+        step!(
+            "verify restore data done{}, kv count: {:?}",
+            if has_learner { " (with learner)" } else { "" },
+            verify_res,
+        );
     }
-    step!(
-        "verify restore data done{}, kv count: {}",
-        if has_learner { " (with learner)" } else { "" },
-        verify_res.unwrap(),
-    );
+
+    // PiTR on restored data.
+    {
+        restore_keyspace::restore_keyspace(
+            keyspace_id,
+            keyspace_id,
+            &instant_backup_name,
+            None,
+            s3fs,
+            cluster.get_pd_client(),
+            runtime,
+            Some(truncate_ts_pitr),
+            reporter,
+        )
+        .unwrap();
+        step!("restore (pitr on restored data) done");
+
+        let verify_res = client
+            .verify_data_with_given_ref_store(
+                &ref_store_pitr,
+                Some((&keyspace_start, &keyspace_end)),
+                &RequestOptions::default(),
+            )
+            .unwrap();
+        step!(
+            "verify (pitr on restored data) done, kv count: {}",
+            verify_res
+        );
+    }
 
     // Write more data to verify the sequences.
     for id in 0..KEYSPACE_COUNT {
@@ -334,7 +400,11 @@ fn test_restore_keyspace_impl(
         } else {
             BASIC_DATA_COUNT
         };
-        client.put_kv(0..count, gen_keyspace_key(id as u32), i_to_val_138);
+        client.put_kv(
+            0..count,
+            gen_keyspace_key(id as u32),
+            i_to_val(FINAL_DATA_LEN),
+        );
     }
     client.verify_data_with_ref_store();
     step!("verify more writes done");
@@ -350,23 +420,23 @@ fn random_val() -> Vec<u8> {
     bytes.to_vec()
 }
 
-// Distinguish different values by length.
-fn i_to_val_138(i: usize) -> Vec<u8> {
-    let mut val = format!("A_{:08}", i).into_bytes();
-    val.append(&mut random_val());
-    val
-}
-
-fn i_to_val_140(i: usize) -> Vec<u8> {
-    let mut val = format!("B_{:010}", i).into_bytes();
-    val.append(&mut random_val());
-    val
-}
-
-fn i_to_val_142(i: usize) -> Vec<u8> {
-    let mut val = format!("C_{:012}", i).into_bytes();
-    val.append(&mut random_val());
-    val
+// Generate `i_to_val` by specifying expected length of value, to make it easier
+// to know where the data is written.
+// `expected_len` must be > 136
+// `expected_len` = 128(random_val) + i(8) + padding
+fn i_to_val(expected_len: usize) -> impl Fn(usize) -> Vec<u8> {
+    assert!(
+        expected_len > 8 + RANDOM_VALUE_LEN,
+        "expected_len: {} is not larger than 136",
+        expected_len
+    );
+    move |i: usize| -> Vec<u8> {
+        let mut val = format!("{:08}", i).into_bytes();
+        let mut padding = vec![b'0'; expected_len - 8 - RANDOM_VALUE_LEN];
+        val.append(&mut padding);
+        val.append(&mut random_val());
+        val
+    }
 }
 
 fn get_keyspace_prefix(keyspace_id: u32) -> Vec<u8> {
@@ -454,6 +524,11 @@ fn check_learners(
         "{:?}",
         check_learners_impl(cluster, start, end, min_count)
     );
+}
+
+fn generate_backup_name() -> String {
+    static BACKUP_ID: AtomicUsize = AtomicUsize::new(0);
+    format!("{:04}", BACKUP_ID.fetch_add(1, Ordering::Relaxed))
 }
 
 #[derive(Default)]
