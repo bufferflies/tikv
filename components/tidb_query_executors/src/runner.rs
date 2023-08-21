d2b8ff930fbd57bc6a07f1be4670bc43c4f9db9b
Meng Huang <791874158@qq.com>
2023-08-21 18:13:22 +0800

    copr: cherry pick tikv master coprocessor changes (#1011)
    * json, copr: implement unary not for json (#14070)
    
    close tikv/tikv#14069
    
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>
    
    * copr: (refactor) Lift heap struct out from top_n_executor (#14096)
    
    ref tikv/tikv#13936
    
    Signed-off-by: Zhi Qi <qizhi@pingcap.com>
    
    * copr: (feat) Implement operator PartitionTopN (#14116)
    
    ref tikv/tikv#13936
    
    Signed-off-by: Zhi Qi <qizhi@pingcap.com>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>
    
    * copr: fix error when cast const Enum to any type (#14149)
    
    close tikv/tikv#14156, close pingcap/tidb#40341
    
    copr: fix error when cast const Enum to any type
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>
    
    * coprocessor: use mur3 to calculate fmsketch (#14204)
    
    ref tikv/tikv#14231
    
    Signed-off-by: xuyifan <675434007@qq.com>
    
    * copr: early stop paging copr when resultset is drained. (#14209)
    
    close tikv/tikv#14254
    
    When the result set is drained, it indicates that no more data is required in the range.
    This PR set the scanned range to None to avoid the following paging requests in the current range.
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>
    
    * coprocessor: avoid unnecessary vec allocation in collect_column_stats (#14280)
    
    ref tikv/tikv#14231
    
    When collect_column_stats handles each row, reuse column_vals and collation_key_vals to avoid allocating many small objects.
    
    Signed-off-by: xuyifan <675434007@qq.com>
    
    * copr: (enhance) support executor limit with partition_by fields (#14359)
    
    ref tikv/tikv#13936
    
    Signed-off-by: Zhi Qi <qizhi@pingcap.com>
    
    * coprocessor: reuse EvalContext in collect_column_stats (#14376)
    
    ref tikv/tikv#14231
    
    Signed-off-by: xuyifan <675434007@qq.com>
    
    * copr: fix extral physical table id when idx key < `MAX_OLD_ENCODED_VALUE_LEN` (#14618)
    
    close tikv/tikv#14619
    
    fix a bug with `process_old_collation_kv` function. 
    
    related with https://github.com/tikv/tikv/pull/11931, forget process `physical_table_id_column_cnt` in process_old_collation_kv function
    
    Signed-off-by: Jason Mo <mohangjie1995@gmail.com>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>
    
    ---------
    
    Signed-off-by: Zhi Qi <qizhi@pingcap.com>
    Signed-off-by: xuyifan <675434007@qq.com>
    Co-authored-by: YangKeao <yangkeao@chunibyo.icu>
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>
    Co-authored-by: Zhi Qi <30543181+LittleFall@users.noreply.github.com>
    Co-authored-by: Shenghui Wu <793703860@qq.com>
    Co-authored-by: Yifan Xu <30385241+xuyifangreeneyes@users.noreply.github.com>
    Co-authored-by: you06 <you1474600@gmail.com>
    Co-authored-by: Hangjie Mo <mohangjie1995@gmail.com>

--- components/tidb_query_executors/src/runner.rs
+++ components/tidb_query_executors/src/runner.rs
@@ -4,6 +4,7 @@ use std::{convert::TryFrom, sync::Arc};
 
 use api_version::KvFormat;
 use fail::fail_point;
+use itertools::Itertools;
 use kvproto::coprocessor::KeyRange;
 use protobuf::Message;
 use tidb_query_common::{
@@ -149,6 +150,15 @@ impl BatchExecutorsRunner<()> {
                 }
                 ExecType::TypePartitionTableScan => {
                     other_err!("PartitionTableScan executor not implemented");
+                }
+                ExecType::TypeSort => {
+                    other_err!("Sort executor not implemented");
+                }
+                ExecType::TypeWindow => {
+                    other_err!("Window executor not implemented");
+                }
+                ExecType::TypeExpand => {
+                    other_err!("Expand executor not implemented");
                 }
             }
         }
@@ -319,14 +329,39 @@ pub fn build_executors<S: Storage + 'static, F: KvFormat>(
             ExecType::TypeLimit => {
                 EXECUTOR_COUNT_METRICS.batch_limit.inc();
 
-                Box::new(
-                    BatchLimitExecutor::new(
-                        executor,
-                        ed.get_limit().get_limit() as usize,
-                        is_src_scan_executor,
-                    )?
-                    .collect_summary(summary_slot_index),
-                )
+                let mut d = ed.take_limit();
+
+                // If there is partition_by field in Limit, we treat it as a
+                // partitionTopN without order_by.
+                // todo: refine those logics.
+                let partition_by = d
+                    .take_partition_by()
+                    .into_iter()
+                    .map(|mut item| item.take_expr())
+                    .collect_vec();
+
+                if partition_by.is_empty() {
+                    Box::new(
+                        BatchLimitExecutor::new(
+                            executor,
+                            d.get_limit() as usize,
+                            is_src_scan_executor,
+                        )?
+                        .collect_summary(summary_slot_index),
+                    )
+                } else {
+                    Box::new(
+                        BatchPartitionTopNExecutor::new(
+                            config.clone(),
+                            executor,
+                            partition_by,
+                            vec![],
+                            vec![],
+                            d.get_limit() as usize,
+                        )?
+                        .collect_summary(summary_slot_index),
+                    )
+                }
             }
             ExecType::TypeTopN => {
                 EXECUTOR_COUNT_METRICS.batch_top_n.inc();
@@ -339,17 +374,36 @@ pub fn build_executors<S: Storage + 'static, F: KvFormat>(
                     order_exprs_def.push(item.take_expr());
                     order_is_desc.push(item.get_desc());
                 }
+                let partition_by = d
+                    .take_partition_by()
+                    .into_iter()
+                    .map(|mut item| item.take_expr())
+                    .collect_vec();
 
-                Box::new(
-                    BatchTopNExecutor::new(
-                        config.clone(),
-                        executor,
-                        order_exprs_def,
-                        order_is_desc,
-                        d.get_limit() as usize,
-                    )?
-                    .collect_summary(summary_slot_index),
-                )
+                if partition_by.is_empty() {
+                    Box::new(
+                        BatchTopNExecutor::new(
+                            config.clone(),
+                            executor,
+                            order_exprs_def,
+                            order_is_desc,
+                            d.get_limit() as usize,
+                        )?
+                        .collect_summary(summary_slot_index),
+                    )
+                } else {
+                    Box::new(
+                        BatchPartitionTopNExecutor::new(
+                            config.clone(),
+                            executor,
+                            partition_by,
+                            order_exprs_def,
+                            order_is_desc,
+                            d.get_limit() as usize,
+                        )?
+                        .collect_summary(summary_slot_index),
+                    )
+                }
             }
             _ => {
                 return Err(other_err!(
@@ -481,9 +535,12 @@ impl<SS: 'static> BatchExecutorsRunner<SS> {
                 self.out_most_executor
                     .collect_exec_stats(&mut self.exec_stats);
 
-                let range = self
-                    .paging_size
-                    .map(|_| self.out_most_executor.take_scanned_range());
+                let range = if drained {
+                    None
+                } else {
+                    self.paging_size
+                        .map(|_| self.out_most_executor.take_scanned_range())
+                };
 
                 let mut sel_resp = SelectResponse::default();
                 sel_resp.set_chunks(chunks.into());
