f1c5701187135468e5c0d6b540b7e490848501e2
iosmanthus <dengliming@pingcap.com>
2024-08-22 15:58:16 +0800

    mtls: log remote_addr while encoutering tls handshake error (#1808)

--- components/cloud_server/src/status_server/mod.rs
+++ components/cloud_server/src/status_server/mod.rs
@@ -1836,7 +1836,10 @@ fn tls_incoming(
             match tokio_openssl::SslStream::new(ssl, stream) {
                 Ok(mut ssl_stream) => match Pin::new(&mut ssl_stream).accept().await {
                     Err(_) => {
-                        error!("Status server error: TLS handshake error");
+                        error!(
+                            "Status server error: TLS handshake error";
+                            "remote_addr" => ssl_stream.get_ref().remote_addr()
+                        );
                         continue;
                     },
                     Ok(()) => {
