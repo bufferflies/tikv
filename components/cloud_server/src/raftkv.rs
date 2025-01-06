65e4dcd9fbad4061c1e099c9281fe36d4de3d41a
glorv <glorvs@163.com>
2025-01-06 12:46:38 +0800

    txn: introduce pessimistic rollback read phase tikv#16185 (#2186)
    Signed-off-by: glorv <glorvs@163.com>

--- components/cloud_server/src/raftkv.rs
+++ components/cloud_server/src/raftkv.rs
@@ -1368,6 +1368,7 @@ mod tests {
                     vec![Key::from_raw(b"k1")],
                     80.into(),
                     80.into(),
+                    None,
                     Context::default(),
                 ),
                 expect_ok_callback(tx.clone(), 1),
