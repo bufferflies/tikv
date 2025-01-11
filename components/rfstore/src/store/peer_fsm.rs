642a22af73cb7745c0a49bf191e8a5e111d8eb3e
glorv <glorvs@163.com>
2025-01-11 09:38:55 +0800

    raftstore: ignore stale read states (tikv#12300) (#2214)
    * raftstore: ignore stale read states (#12300)
    
    close tikv/tikv#12046
    
    If a read index request travel too long in the network, a leader may receive the
    request it sent when it's still follower and break the strict order pending read
    assumption.
    
    This PR solves the problem by ignoring invalid read states. It also adds a defense
    check to ignore all ReadIndex messages when it's sent by current leader.
    
    Signed-off-by: Jay Lee <BusyJayLee@gmail.com>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>
    
    * ignore stale read states for rfstore
    
    Signed-off-by: glorv <glorvs@163.com>
    
    ---------
    
    Signed-off-by: glorv <glorvs@163.com>
    Co-authored-by: Jay <BusyJay@users.noreply.github.com>
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>

--- components/rfstore/src/store/peer_fsm.rs
+++ components/rfstore/src/store/peer_fsm.rs
@@ -599,6 +599,16 @@ impl<'a> PeerMsgHandler<'a> {
             self.on_transfer_leader_msg(msg.get_message());
             Ok(())
         } else {
+            // This can be a message that sent when it's still a follower. Nevertheleast,
+            // it's meaningless to continue to handle the request as callbacks are cleared.
+            if msg.get_message().get_msg_type() == MessageType::MsgReadIndex
+                && self.fsm.peer.is_leader()
+                && (msg.get_message().get_from() == raft::INVALID_ID
+                    || msg.get_message().get_from() == self.fsm.peer_id())
+            {
+                self.ctx.raft_metrics.message_dropped.stale_msg.inc();
+                return Ok(());
+            }
             self.fsm.peer.step(self.ctx, msg.take_message())
         };
 
