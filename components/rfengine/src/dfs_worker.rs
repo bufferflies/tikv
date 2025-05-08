17a8a0e9bec9acb7530014f1825262166b3b9128
Ping Yu <yuping@pingcap.com>
2025-05-08 15:59:18 +0800

    native-br: Support backup&restore rlog file with compression (cp #2712) (#2737)
    * support backup&restore rlog file with compression (#2712)
    
    Signed-off-by: 3pointer <luancheng@pingcap.com>
    Signed-off-by: Ping Yu <yuping@pingcap.com>
    
    * polish
    
    Signed-off-by: Ping Yu <yuping@pingcap.com>
    
    ---------
    
    Signed-off-by: 3pointer <luancheng@pingcap.com>
    Signed-off-by: Ping Yu <yuping@pingcap.com>
    Co-authored-by: 3pointer <luancheng@pingcap.com>
    Co-authored-by: Evan Zhou <coocood@gmail.com>

--- components/rfengine/src/dfs_worker.rs
+++ components/rfengine/src/dfs_worker.rs
@@ -29,6 +29,9 @@ pub(crate) struct LightweightBackupConfig {
     pub(crate) dir: PathBuf,
     pub(crate) wal_chunk_target_file_size: usize,
     pub(crate) compression_type: CompressionType,
+    // for upgrade compatibility, set rlog_compression_type to false first
+    // so we need another compress configuration.
+    pub(crate) rlog_compression_type: CompressionType,
     pub(crate) dfs_config: DFSConfig,
 
     pub(crate) rlog_cache_capacity: usize,
@@ -41,6 +44,7 @@ impl LightweightBackupConfig {
         dir: PathBuf,
         wal_chunk_target_file_size: usize,
         compression_type: CompressionType,
+        rlog_compression_type: CompressionType,
         dfs_config: DFSConfig,
         rlog_cache_capacity: usize,
         rlog_cache_size_threshold: usize,
@@ -50,6 +54,7 @@ impl LightweightBackupConfig {
             dir,
             wal_chunk_target_file_size,
             compression_type,
+            rlog_compression_type,
             dfs_config,
             rlog_cache_capacity,
             rlog_cache_size_threshold,
@@ -712,6 +717,7 @@ mod tests {
                 std::env::temp_dir(),
                 1024 * 1024,
                 CompressionType::Lz4Compression,
+                CompressionType::NoCompression,
                 dfs_config,
                 1024 * 1024,
                 4096,
--- components/rfengine/src/engine.rs
+++ components/rfengine/src/engine.rs
@@ -1,6 +1,7 @@
 // Copyright 2021 TiKV Project Authors. Licensed under Apache-2.0.
 
 use std::{
+    borrow::Cow,
     collections::{BTreeMap, HashMap, HashSet},
     fmt::{Display, Formatter},
     fs,
@@ -233,6 +234,7 @@ impl RfEngineCore {
                         dir.to_owned(),
                         cfg.wal_chunk_target_file_size.0 as usize,
                         CompressionType::Lz4Compression,
+                        CompressionType::NoCompression,
                         dfs_conf.unwrap(),
                         cfg.rlog_cache_capacity.0 as usize,
                         cfg.rlog_cache_size_threshold.0 as usize,
@@ -718,6 +720,16 @@ fn restore_all_raft_logs(
     restore_all_raft_logs_with_snap_rlog_file(None, store_meta, dir, rlog_data)
 }
 
+fn decompress_snap_rlog_file(compression_type: u32, content: &[u8]) -> Result<Cow<'_, [u8]>> {
+    match CompressionType::from(compression_type) {
+        CompressionType::Lz4Compression => {
+            let decompressed = decompress_lz4(content).map_err(Error::from)?;
+            Ok(Cow::Owned(decompressed))
+        }
+        CompressionType::NoCompression => Ok(Cow::Borrowed(content)),
+    }
+}
+
 fn restore_all_raft_logs_with_snap_rlog_file(
     keyspace_id: Option<u32>,
     store_meta: &StoreBackupMeta,
@@ -727,18 +739,20 @@ fn restore_all_raft_logs_with_snap_rlog_file(
     let mut raft_meta = StoreRaftLogBackupMeta::default();
     let size = rlog_data.len();
     debug_assert!(size as u64 > store_meta.raft_meta_start_off);
+
     raft_meta
         .merge_from_bytes(&rlog_data.chunk()[store_meta.raft_meta_start_off as usize..size])
         .unwrap();
+    let compression_type = raft_meta.get_header().get_compression_type();
 
     let handle_keyspace = |keyspace_meta: &KeySpaceBackupMeta| -> Result<()> {
         for file in keyspace_meta.get_files() {
             let path = raft_log_file_name(dir, file.peer_id, file.first_index, file.last_index);
-            fs::write(
-                &path,
+            let content = decompress_snap_rlog_file(
+                compression_type,
                 &rlog_data.chunk()[file.start_off as usize..file.end_off as usize],
-            )
-            .with_ctx(|| format!("write rlog {}", path.display()))?;
+            )?;
+            fs::write(&path, content).with_ctx(|| format!("write rlog {}", path.display()))?;
         }
         Ok(())
     };
