5ad4febe3691c637ed770b0bf17d9386f14290fc
Evan Zhou <coocood@gmail.com>
2025-07-22 09:55:05 +0800

    config: reduce default grpc concurrency factor (#3153)
    Most of the workload doesn't need that much of grpc threads even if get is executed.
    
    Signed-off-by: Evan Zhou <coocood@gmail.com>

--- src/server/config.rs
+++ src/server/config.rs
@@ -238,7 +238,7 @@ impl Default for Config {
             grpc_min_message_size_to_compress: DEFAULT_GRPC_MIN_MESSAGE_SIZE_TO_COMPRESS,
             grpc_concurrency: DEFAULT_GRPC_CONCURRENCY,
             grpc_concurrent_stream: DEFAULT_GRPC_CONCURRENT_STREAM,
-            grpc_concurrency_factor: 0.75, // 75% of CPU cores
+            grpc_concurrency_factor: 0.5, // 50% of CPU cores
             grpc_raft_conn_num: DEFAULT_GRPC_RAFT_CONN_NUM,
             grpc_stream_initial_window_size: ReadableSize(DEFAULT_GRPC_STREAM_INITIAL_WINDOW_SIZE),
             grpc_memory_pool_quota: ReadableSize(DEFAULT_GRPC_MEMORY_POOL_QUOTA),
